d129739659fd9acb1995a642ac0502354f40b797
vitaliy <vitaliy.byvalin@mail.ru>
2020-05-26 19:10:35 +0300

    promt and object praktic

--- js/style.js
+++ js/style.js
@@ -1,6 +1,26 @@
 "use strict";
-let a=18;
-console.log(a);
-let number=15;
+
+
+
 //alert(' ну здорово');
-confirm('are yuo here???');
+//confirm('are yuo here???');
+
+const NumberOfFilms= +prompt('сколько фильмов вы уже посмотрели' , '');
+console.log(NumberOfFilms);
+
+
+const PersonalMovieDB = {
+   count : NumberOfFilms,
+   movies : {},
+   actors : {},
+   genres : [],
+   private : false
+};
+
+const a = prompt('один из последних просмотренных фильмов?' , '');
+const b = prompt('на сколько оцените его', '');
+const c = prompt('один из последних просмотренных фильмов?' , '');
+const d = prompt('на сколько оцените его', '');
+PersonalMovieDB.movies[a] = b;
+PersonalMovieDB.movies[c] = d;
+console.log(PersonalMovieDB);
