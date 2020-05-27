674f252472685ba146ed08e6ead5df902c394ce3
vitaliy <vitaliy.byvalin@mail.ru>
2020-05-27 22:29:08 +0300

    swithcase

--- js/style.js
+++ js/style.js
@@ -5,22 +5,74 @@
 //alert(' ну здорово');
 //confirm('are yuo here???');
 
-const NumberOfFilms= +prompt('сколько фильмов вы уже посмотрели' , '');
-console.log(NumberOfFilms);
+
+                    // ПЕРВАЯ ДОМАШКА
+ const NumberOfFilms= +prompt('сколько фильмов вы уже посмотрели' , '');
+ console.log(NumberOfFilms);
 
 
-const PersonalMovieDB = {
+ let PersonalMovieDB = {
    count : NumberOfFilms,
-   movies : {},
-   actors : {},
-   genres : [],
-   private : false
-};
+    movies : {},
+    actors : {},
+    genres : [],
+    private : false
+ };
 
-const a = prompt('один из последних просмотренных фильмов?' , '');
-const b = prompt('на сколько оцените его', '');
-const c = prompt('один из последних просмотренных фильмов?' , '');
-const d = prompt('на сколько оцените его', '');
-PersonalMovieDB.movies[a] = b;
-PersonalMovieDB.movies[c] = d;
-console.log(PersonalMovieDB);
+// const a = prompt('один из последних просмотренных фильмов?' , '');
+// const b = prompt('на сколько оцените его', '');
+// const c = prompt('один из последних просмотренных фильмов?' , '');
+// const d = prompt('на сколько оцените его', '');
+// PersonalMovieDB.movies[a] = b;
+// PersonalMovieDB.movies[c] = d;
+// console.log(PersonalMovieDB);
+              //-----------------------------------------
+
+              // SWITCH case
+
+            //   let side = 888;
+
+            //   switch (side) {
+            //      case 50:
+            //         console.log('это 50');
+            //         break;
+
+            //       case 888:
+            //          console.log('сторона равна 888');
+            //          break;
+
+            //       default:
+            //             console.log('нужная сторона не найдена');
+            //             break;
+            //   }
+            //------------------------------------------------
+
+           // ЦИКЛЫ
+
+           for(let i = 0; i < 2; i++){
+            const a = prompt('один из последних просмотренных фильмов?' , '');
+            const b = prompt('на сколько оцените его', '');
+            if(a != null && b != null && a != '' && b != '' && a.length < 50 ){
+               PersonalMovieDB.movies[a] = b;
+               console.log('done');
+            }
+            else{
+               console.log('error');
+               i--;
+            }
+           }
+
+           if(PersonalMovieDB.count < 10){
+              console.log('просмотренно довольно мало фильмов');              
+           }
+              else if (PersonalMovieDB.count >= 10 && PersonalMovieDB.count < 30){
+               console.log('вы классический зритель');
+              }
+              else if(PersonalMovieDB.count >= 30){
+               console.log('вы киноман');
+              }
+              else{
+               console.log('произошла ошибка');
+              }
+
+              console.log(PersonalMovieDB);
